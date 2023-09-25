8c5ec5a64284a517e71d68f4bf058d016081eaaa
joshunrau <contact@joshuaunrau.com>
2023-09-25 16:29:10 -0400

    rm dead code

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -43,41 +43,4 @@ export const IndexPage = () => {
       </motion.div>
     </div>
   );
-
-  return {
-    /* <main className="container flex flex-grow items-center justify-center">
-        <section className="flex gap-5 text-center xl:text-left w-full">
-          <motion.div
-            animate={{ opacity: 1, y: 0 }}
-            className="flex flex-grow flex-col justify-center"
-            initial={{ opacity: 0, y: 10 }}
-            transition={{ duration: 0.7 }}
-          >
-            <h1 className="text-3xl font-bold tracking-tight text-slate-900 dark:text-slate-100 md:text-4xl lg:text-5xl">
-              {t('heroTitle')}
-            </h1>
-            <p className="mx-auto mt-3 w-11/12 text-base text-slate-600 dark:text-slate-300 md:mt-5 md:text-lg xl:mx-0">
-              {t('heroContent')}
-            </p>
-            <div className="mt-5 flex justify-center gap-3 xl:justify-start">
-              <Button
-                label={t('accessMetadata')}
-                type="button"
-                onClick={() => {
-                  return;
-                }}
-              />
-            </div>
-          </motion.div>
-          <motion.div
-            animate={{ opacity: 1, x: 0, y: 0 }}
-            className="hidden w-80 xl:block"
-            initial={{ opacity: 0, x: 10, y: 10 }}
-            transition={{ duration: 0.7 }}
-          >
-            <HeroIcon />
-          </motion.div>
-        </section>
-      </main> */
-  };
 };
